fc232733190b0c32c9c31369ec7c86bbb7195593
kouichi.takeyama <takeshan2001@yahoo.co.jp>
2020-07-22 21:19:21 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 竹山　晃一 on 2020/07/22.
+//  Copyright © 2020 kouichi.takeyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
